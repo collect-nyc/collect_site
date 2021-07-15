0ceaa3a491a7b5367b6269e653cc317b031998fb
Joshua Tuscan <hello@joshuatuscan.com>
2021-07-15 17:22:25 -0400

    fixing className error on nav

--- components/SiteNav.js
+++ components/SiteNav.js
@@ -1,15 +1,13 @@
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
-import { getAllArchives } from '../lib/api';
-import styles from '../styles/Nav.module.scss';
-
-
+import { getAllArchives } from "../lib/api";
+import styles from "../styles/Nav.module.scss";
 
 const SiteNav = () => {
   // do some stuff
 
   return (
-    <nav className={styles.navigation + " site_nav"}>
+    <nav className={styles.navigation}>
       <Link href="/profile">
         <a className={styles.link_box}>
           <span>COLLECT Archive</span>
