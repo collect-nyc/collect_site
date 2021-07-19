6130d7dfed19a9c2098cac2765a5c540a63b83cf
Joshua Tuscan <hello@joshuatuscan.com>
2021-07-19 15:48:08 -0400

    building out the project viewer on project page

--- components/ProjectViewer.js
+++ components/ProjectViewer.js
@@ -4,28 +4,54 @@ import { motion } from "framer-motion";
 import styles from "../styles/ProjectViewer.module.scss";
 
 const ProjectViewer = ({ images, embeds }) => {
-  console.log("ProjectViewer", images, embeds);
   const [items, setItems] = useState(null);
 
-  useEffect(() => {
-    let combined = null;
+  console.log("IMAGES", images, embeds);
 
-    if (images[0].image && embeds[0].embed) {
-      console.log("Both images and embeds");
-    } else if (images[0].image) {
-      console.log("Only images");
-    } else if (embeds[0].embed) {
-      console.log("Only embeds");
-    } else {
-      console.log("Only embeds");
-    }
+  // useEffect(() => {
+  //   let combined = null;
 
-    combined = images;
+  //   if (images[0].image && embeds[0].embed) {
+  //     console.log("Both images and embeds");
 
-    setItems(combined);
-  }, [images, embeds]);
+  //     combined = {
+  //       ...images,
+  //       ...embeds,
+  //     };
+  //   } else if (images[0].image) {
+  //     console.log("Only images");
+  //     combined = images;
+  //   } else if (embeds[0].embed) {
+  //     console.log("Only embeds");
+  //     combined = embeds;
+  //   }
 
-  return <div className={styles.project_viewer}></div>;
+  //   console.log("ProjectViewer", combined);
+
+  //   setItems(combined);
+  // }, [images, embeds]);
+
+  return (
+    <div className={styles.project_viewer}>
+      <ul className={styles.image_array}>
+        {images[0].image
+          ? images.map((image, key) => (
+              <li key={key}>
+                {/*<Image
+                  height={image.image.dimensions.height}
+                  width={image.image.dimensions.width}
+                  layout={"fill"}
+                  src={image.image.url}
+                  alt={image.image.alt}
+                />*/}
+                <img src={image.image.url} alt={image.image.alt} />
+              </li>
+            ))
+          : null}
+      </ul>
+      <div className={styles.holder}></div>
+    </div>
+  );
 };
 
 export default ProjectViewer;
