22e23b95d7d1ef8c090b9d248e7442db05e4d6ce
Joshua Tuscan <hello@joshuatuscan.com>
2021-07-19 17:23:34 -0400

    working carousel for moving through project media

--- components/ProjectViewer.js
+++ components/ProjectViewer.js
@@ -1,19 +1,37 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import { motion } from "framer-motion";
 import styles from "../styles/ProjectViewer.module.scss";
 
 const ProjectViewer = ({ images, embeds }) => {
-  const [items, setItems] = useState(null);
+  const [current, setCurrent] = useState(0);
+
+  const total = images.length;
 
   const nextItem = () => {
     // something
     console.log("NEXT ITEM");
+
+    if (current + 1 >= total) {
+      setCurrent(0);
+    } else {
+      setCurrent(current + 1);
+    }
+
+    console.log("CURRENT", current);
   };
 
   const prevItem = () => {
     // something
     console.log("PREVIOUS ITEM");
+
+    if (current === 0) {
+      setCurrent(total - 1);
+    } else {
+      setCurrent(current - 1);
+    }
+
+    console.log("CURRENT", current);
   };
 
   console.log("IMAGES", images);
@@ -23,7 +41,11 @@ const ProjectViewer = ({ images, embeds }) => {
       <ul className={styles.image_array}>
         {images[0].image
           ? images.map((image, key) => (
-              <li key={key}>
+              <li
+                key={key}
+                index={key}
+                className={key === current ? styles.current : styles.hidden}
+              >
                 {/*<Image
                   height={image.image.dimensions.height}
                   width={image.image.dimensions.width}
@@ -31,6 +53,19 @@ const ProjectViewer = ({ images, embeds }) => {
                   src={image.image.url}
                   alt={image.image.alt}
                 />*/}
+
+                {images.length > 1 ? (
+                  <React.Fragment>
+                    <button
+                      onClick={() => prevItem()}
+                      className={styles.prev_btn}
+                    />
+                    <button
+                      onClick={() => nextItem()}
+                      className={styles.next_btn}
+                    />
+                  </React.Fragment>
+                ) : null}
 
                 {image.image ? (
                   <Image
@@ -46,15 +81,6 @@ const ProjectViewer = ({ images, embeds }) => {
                     Your browser does not support the video tag.
                   </video>
                 ) : null}
-
-                <button
-                  onClick={() => prevItem()}
-                  className={styles.prev_btn}
-                />
-                <button
-                  onClick={() => nextItem()}
-                  className={styles.next_btn}
-                />
               </li>
             ))
           : null}
