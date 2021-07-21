11fc6d1418997d9a4f6e5e03ca01bc908fba4e18
Joshua Tuscan <hello@joshuatuscan.com>
2021-07-21 18:44:26 -0400

    adding lodash and creating layout view switcher

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import { DateTime } from "luxon";
 import Image from "next/image";
 import Link from "next/link";
 import axios from "axios";
+import _ from "lodash";
 import styles from "../styles/Index.module.scss";
 
 export async function getServerSideProps() {
@@ -24,11 +25,15 @@ export async function getServerSideProps() {
 
 const Home = ({ data, archives, taggers }) => {
   const page_content = data[0].node;
+
+  // State
+  const [gridView, setGridView] = useState(false);
   const [archiveList, setArchiveList] = useState(archives);
   const [currentTag, setCurrentTag] = useState(null);
 
   console.log("DATA", page_content, "ARCHIVES", archives, "TAGS", taggers);
 
+  // Pull archive items by tag
   const GetByTag = (id, name) => {
     // do something
     console.log("TAG CLICK", id);
@@ -47,6 +52,52 @@ const Home = ({ data, archives, taggers }) => {
       .catch(function (error) {
         console.log(error);
       });
+  };
+
+  const SwapView = () => {
+    setGridView(!gridView);
+  };
+
+  const ListView = () => {
+    return archiveList.length > 0 ? (
+      archiveList.map((archive, key) => (
+        <li key={key}>
+          <Link href={"/item/" + archive.node._meta.uid}>
+            <a>
+              <span className={styles.name}>{archive.node.title[0].text}</span>
+
+              <span className={styles.tags}>
+                {archive.node.tags.map((item, key) => (
+                  <span key={key}>
+                    {archive.node.tags.length === key + 1 && item.tag
+                      ? item.tag.tag_name[0].text
+                      : item.tag
+                      ? item.tag.tag_name[0].text + ", "
+                      : null}
+                  </span>
+                ))}
+              </span>
+
+              <span className={styles.date}>
+                {archive.node.creation_date
+                  ? DateTime.fromISO(archive.node.creation_date).toFormat(
+                      "yyyy"
+                    )
+                  : "TBD"}
+              </span>
+            </a>
+          </Link>
+        </li>
+      ))
+    ) : (
+      <li className={styles.no_items}>
+        No &ldquo;{currentTag}&rdquo; items found.
+      </li>
+    );
+  };
+
+  const GridView = () => {
+    return <h1>Grid View</h1>;
   };
 
   return (
@@ -80,7 +131,9 @@ const Home = ({ data, archives, taggers }) => {
         </ul>
 
         <span className={styles.controls}>
-          <button>Grid</button>
+          <button onClick={() => SwapView()}>
+            {gridView ? "List" : "Grid"}
+          </button>
           <button>A-Z</button>
           <button>New, Old</button>
         </span>
@@ -93,45 +146,7 @@ const Home = ({ data, archives, taggers }) => {
         </h1>
 
         <section className={styles.all_archives}>
-          <ul>
-            {archiveList.length > 0 ? (
-              archiveList.map((archive, key) => (
-                <li key={key}>
-                  <Link href={"/item/" + archive.node._meta.uid}>
-                    <a>
-                      <span className={styles.name}>
-                        {archive.node.title[0].text}
-                      </span>
-
-                      <span className={styles.tags}>
-                        {archive.node.tags.map((item, key) => (
-                          <span key={key}>
-                            {archive.node.tags.length === key + 1 && item.tag
-                              ? item.tag.tag_name[0].text
-                              : item.tag
-                              ? item.tag.tag_name[0].text + ", "
-                              : null}
-                          </span>
-                        ))}
-                      </span>
-
-                      <span className={styles.date}>
-                        {archive.node.creation_date
-                          ? DateTime.fromISO(
-                              archive.node.creation_date
-                            ).toFormat("yyyy")
-                          : "TBD"}
-                      </span>
-                    </a>
-                  </Link>
-                </li>
-              ))
-            ) : (
-              <li className={styles.no_items}>
-                No &ldquo;{currentTag}&rdquo; items found.
-              </li>
-            )}
-          </ul>
+          <ul>{gridView ? <GridView /> : <ListView />}</ul>
         </section>
       </main>
       <footer className={styles.footer}>
