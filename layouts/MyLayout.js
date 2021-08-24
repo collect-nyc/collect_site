f46058c0536a02aaa9ee858e0b85a84b843f16d0
Joshua Tuscan <hello@joshuatuscan.com>
2021-08-24 15:51:48 -0400

    turning off logs

--- layouts/MyLayout.js
+++ layouts/MyLayout.js
@@ -7,7 +7,7 @@ const fetcher = (url) => fetch(url).then((res) => res.json());
 export default function MyLayout({ page, children }) {
   const { data, error } = useSWR("/api/get-count", fetcher);
 
-  console.log("MYLAYOUT", data);
+  // console.log("MYLAYOUT", data);
 
   const totalCount = data ? data.count + data.media : null;
 
