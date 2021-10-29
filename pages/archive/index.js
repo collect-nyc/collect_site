896e13f62ecb9217028b4a675d35b46cf7176460
Joshua Tuscan <hello@joshuatuscan.com>
2021-10-29 14:41:59 -0400

    merged v2

--- pages/archive/index.js
+++ pages/archive/index.js
@@ -469,9 +469,6 @@ const Home = ({ archives, document, tagged }) => {
           {layoutView ? <GridView /> : <ListView />}
         </div>
       </main>
-<<<<<<< HEAD
-      <footer className={styles.footer}></footer>
-=======
       <footer className={styles.footer}>
         <div className={styles.copyright}>
           ©{new Date().getFullYear()} Collect NYC, All Rights Reserved
@@ -481,7 +478,6 @@ const Home = ({ archives, document, tagged }) => {
           <Link href="/privacy">Impressum</Link>
         </div>
       </footer>
->>>>>>> d7b570690695582668af4598d59d3c382654f960
     </div>
   );
 };
