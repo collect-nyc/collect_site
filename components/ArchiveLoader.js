33e59e5d740da4d7398732997299456f3868a4db
Joshua Tuscan <hello@joshuatuscan.com>
2022-05-12 14:29:41 -0400

    setting up image arrays

--- components/ArchiveLoader.js
+++ components/ArchiveLoader.js
@@ -14,15 +14,37 @@ export default function Loader({ data }) {
     right_images,
   } = data;
 
+  const imgStyle = (imgSrc) => ({
+    backgroundImage: `url(${imgSrc})`,
+  });
+
   return (
     <section className={styles.loader_section}>
       <div className={styles.left_side}>
+        {left_images && left_images.length > 0 ? (
+          <ul className={styles.image_array}>
+            {left_images.map((image, index) => (
+              <li style={imgStyle(image.image.url)} key={index}>
+                {index}
+              </li>
+            ))}
+          </ul>
+        ) : null}
         <p>
           <span>{left_text_line_one && left_text_line_one}</span>
           <span>{left_text_line_two && left_text_line_two}</span>
         </p>
       </div>
       <div className={styles.right_side}>
+        {right_images && right_images.length > 0 ? (
+          <ul className={styles.image_array}>
+            {right_images.map((image, index) => (
+              <li style={imgStyle(image.image.url)} key={index}>
+                {index}
+              </li>
+            ))}
+          </ul>
+        ) : null}
         <p>
           <span>{right_text_line_one && right_text_line_one}</span>
           <span>{right_text_line_two && right_text_line_two}</span>
