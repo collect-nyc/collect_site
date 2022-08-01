e659dc14f099c14b57db27b21aca47873081d86d
Joshua Tuscan <hello@joshuatuscan.com>
2022-08-01 10:42:17 -0400

    turning off log

--- pages/index.js
+++ pages/index.js
@@ -52,7 +52,7 @@ export async function getServerSideProps({ query }) {
 
 const Home = ({ document }) => {
   const router = useRouter();
-  console.log("Landing Data", document.data);
+  // console.log("Landing Data", document.data);
 
   // console.log("featured data", document.data.body1);
 
