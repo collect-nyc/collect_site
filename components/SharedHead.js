c99a8d223b5b15e372ff3829b780424488ac0484
Joshua Tuscan <hello@joshuatuscan.com>
2022-12-20 16:20:10 -0500

    new favicon

--- components/SharedHead.js
+++ components/SharedHead.js
@@ -21,9 +21,9 @@ const SharedHead = () => {
         href="/favicon-16x16.png"
       />
       <link rel="manifest" href="/site.webmanifest" />
-      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
-      <meta name="msapplication-TileColor" content="#fafafa" />
-      <meta name="theme-color" content="#fafafa" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+      <meta name="msapplication-TileColor" content="#ffffff" />
+      <meta name="theme-color" content="#ffffff" />
 
       <script
         async
