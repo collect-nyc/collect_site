203b794af6a627b5f4a8ad956cf79d829089e733
Joshua Tuscan <hello@joshuatuscan.com>
2023-04-04 13:13:14 -0400

    trying to fix sass loader error

--- next.config.js
+++ next.config.js
@@ -1,14 +1,9 @@
-const path = require("path");
 const withSvgr = require("next-svgr");
 
 module.exports = withSvgr({
   images: {
     domains: ["images.prismic.io", "collectnyc.cdn.prismic.io"],
     formats: ["image/avif", "image/webp"],
-  },
-  sassOptions: {
-    fiber: false,
-    includePaths: [path.join(__dirname, "styles")],
   },
   reactStrictMode: true,
 });
