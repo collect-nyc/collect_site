857f5d4ef6a17e728f2daae9df650e7f431fed12
Joshua Tuscan <hello@joshuatuscan.com>
2023-04-06 11:43:24 -0400

    home page footer updates

--- components/HomeFooter.js
+++ components/HomeFooter.js
@@ -6,18 +6,19 @@ import styles from "./HomeFooter.module.scss";
 const Footer = () => {
   return (
     <footer className={styles.footer}>
+      <div className={styles.top_border} />
       <div className={styles.contact}>
         <div className={styles.col}>
+          <span className={styles.address}>
+            100A Broadway #377
+            <br /> Brooklyn, NY 11249
+          </span>
           <div className={styles.reachout}>
             <span className={styles.phone}>
               <a href="mailto:info@collect.nyc">info@collect.nyc</a>
             </span>
             <span className={styles.phone}>+1 718 902 4911</span>
           </div>
-          <span className={styles.address}>
-            100A Broadway #377
-            <br /> Brooklyn, NY 11249
-          </span>
         </div>
 
         <div className={styles.col}>
