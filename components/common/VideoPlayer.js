c0c411b9951e4edb44f5b1e813ef1b1c3426fbcd
Joshua Tuscan <hello@joshuatuscan.com>
2023-05-11 12:41:11 -0400

    fixing homepage linear rules

--- components/common/VideoPlayer.js
+++ components/common/VideoPlayer.js
@@ -2,22 +2,26 @@ import React, { useEffect, useRef, useState } from "react";
 
 const VideoPlayer = ({ source }) => {
   const videoRef = useRef(null);
-  // const [isLoaded, setIsLoaded] = useState(false);
+  const [isLoaded, setIsLoaded] = useState(false);
 
-  // const handleLoadedData = () => {
-  //   setIsLoaded(true);
-  // };
+  const handleLoadedData = () => {
+    setIsLoaded(true);
+  };
 
-  // useEffect(() => {
-  //   if (isLoaded) {
-  //     console.log("Video is loaded", source);
-  //     // videoRef.current.play();
-  //   }
-  // }, [isLoaded]);
+  useEffect(() => {
+    if (isLoaded) {
+      console.log("Video is loaded", source);
+      videoRef.current.play();
+    }
+
+    return () => {
+      setIsLoaded(false);
+    };
+  }, [isLoaded]);
 
   useEffect(() => {
     if (videoRef.current && videoRef.current.paused) {
-      // console.log("Video is paused", source);
+      console.log("Video is paused", source);
       videoRef.current.play();
     }
   }, []);
