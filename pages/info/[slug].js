7419e8d1e05baab70f9cc4559a2e3e978f2de062
Joshua Tuscan <hello@joshuatuscan.com>
2023-11-28 16:39:28 -0500

    removing apollo and graphql

--- pages/info/[slug].js
+++ pages/info/[slug].js
@@ -1,35 +1,22 @@
 import React, { useRef } from "react";
 import Head from "next/head";
-import Prismic from "prismic-javascript";
 import _ from "lodash";
 import { RichText } from "prismic-reactjs";
-import { gql } from "@apollo/client";
 import SharedHead from "../../components/SharedHead";
 import Footer from "../../components/Footer";
 import MyLayout from "../../layouts/MyLayout";
 import { Client } from "../../lib/prismic-config";
-import { apolloClient } from "../../lib/apollo-config";
 import { motion, useTransform, useViewportScroll } from "framer-motion";
 import { SITE_NAME } from "../../lib/constants";
 import styles from "./EssentialText.module.scss";
 
-export async function getStaticProps({ params, preview = false, previewData }) {
+export async function getServerSideProps({
+  params,
+  preview = false,
+  previewData,
+}) {
   const uid = params.slug;
-  const { loading, error, data } = await apolloClient.query({
-    query: gql`
-      query essential_text($uid: String!) {
-        essential_text(lang: "en-us", uid: $uid) {
-          text
-          meta_title
-          meta_description
-          meta_image
-        }
-      }
-    `,
-    variables: {
-      uid: params.slug,
-    },
-  });
+  const data = await Client().getByUID("essential_text", uid);
 
   // console.log(data);
 
@@ -39,30 +26,10 @@ export async function getStaticProps({ params, preview = false, previewData }) {
   return { props: { document, page, uid, revalidate: 60 } };
 }
 
-export async function getStaticPaths() {
-  const essential_text = await Client().query(
-    Prismic.Predicates.at("document.type", "essential_text"),
-    { pageSize: 100 }
-  );
-
-  const posts = essential_text.results;
-
-  // console.log(posts);
-
-  const paths = posts.map((post) => ({
-    params: {
-      slug: post.uid,
-    },
-  }));
-  // console.log(paths);
-  return { paths, fallback: "blocking" };
-}
-
 const EssentialText = ({ document, uid }) => {
-  const { meta_title, meta_description, meta_image, text } =
-    document.essential_text;
+  const { meta_title, meta_description, meta_image, text } = document.data;
 
-  // console.log("Project Data", document.essential_text);
+  console.log("Project Data", document);
 
   const ref = useRef();
   const { scrollYProgress } = useViewportScroll(ref);
