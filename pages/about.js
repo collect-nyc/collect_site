101ff8449d3d16c2ed839e0a7b90d6191fcd94be
Joshua Tuscan <hello@joshuatuscan.com>
2023-12-05 18:52:40 -0500

    about nav

--- pages/about.js
+++ pages/about.js
@@ -59,122 +59,170 @@ const About = ({ data }) => {
       </Head>
 
       <main className={styles.main}>
-        <section className={styles.statement}>
-          <div className={styles.textbox}>
-            {statement ? <PortableText value={statement} /> : null}
-          </div>
-        </section>
-        <section className={`${styles.section} ${styles.newbusiness}`}>
-          <div className={styles.inner}>
-            <span className={styles.label}>New Business</span>
-            <div className={styles.content}>
-              <PortableText value={newbusiness} />
-              <p>
-                <a href="mailto:new@collect.nyc">new@collect.nyc</a>
-                <br />
-                +1 718 902 4911
-              </p>
+        <article>
+          <section className={styles.statement}>
+            <div className={styles.textbox}>
+              {statement ? <PortableText value={statement} /> : null}
             </div>
-          </div>
-        </section>
-        <section className={`${styles.section} ${styles.hiring}`}>
-          <div className={styles.inner}>
-            <span className={styles.label}>Work Opportunities</span>
-            <div className={styles.content}>
-              <PortableText value={hiring} />
-              <p>
-                <a href="mailto:info@collect.nyc">info@collect.nyc</a>
-                <br />
-                <a target="_blank" href="https://www.instagram.com/collect.nyc">
-                  @collect.nyc
-                </a>
-              </p>
+          </section>
+          <section className={`${styles.section} ${styles.newbusiness}`}>
+            <div className={styles.inner}>
+              <span className={styles.label}>New Business</span>
+              <div className={styles.content}>
+                <PortableText value={newbusiness} />
+                <p>
+                  <a href="mailto:new@collect.nyc">new@collect.nyc</a>
+                  <br />
+                  +1 718 902 4911
+                </p>
+              </div>
             </div>
-          </div>
-        </section>
-        <section className={`${styles.section} ${styles.founders}`}>
-          <div className={styles.inner}>
-            <span className={styles.label}>Founders, Principals</span>
-            <div className={styles.content}>
-              {founders.map((founder, i) => (
-                <div className={styles.founder} key={i}>
-                  <span>{founder.Name}</span>
-                  <span>{founder.Title}</span>
-                </div>
-              ))}
+          </section>
+          <section className={`${styles.section} ${styles.hiring}`}>
+            <div className={styles.inner}>
+              <span className={styles.label}>Work Opportunities</span>
+              <div className={styles.content}>
+                <PortableText value={hiring} />
+                <p>
+                  <a href="mailto:info@collect.nyc">info@collect.nyc</a>
+                  <br />
+                  <a
+                    target="_blank"
+                    href="https://www.instagram.com/collect.nyc"
+                  >
+                    @collect.nyc
+                  </a>
+                </p>
+              </div>
             </div>
-          </div>
-        </section>
-        <section className={`${styles.section} ${styles.socials}`}>
-          <div className={styles.inner}>
-            <span className={styles.label}>Now Online</span>
-            <div className={styles.content}>
-              <ul>
-                {instagram.map((instalink, i) => (
-                  <li key={i}>
-                    <a
-                      className={styles.instalink}
-                      href={instalink.URL}
-                      target="_blank"
-                    >
-                      {instalink.Account}
-                    </a>
-                  </li>
+          </section>
+          <section className={`${styles.section} ${styles.founders}`}>
+            <div className={styles.inner}>
+              <span className={styles.label}>Founders, Principals</span>
+              <div className={styles.content}>
+                {founders.map((founder, i) => (
+                  <div className={styles.founder} key={i}>
+                    <span>{founder.Name}</span>
+                    <span>{founder.Title}</span>
+                  </div>
                 ))}
-              </ul>
-
-              <ul>
-                {sociallinks.map((sociallink, i) => (
-                  <li key={i}>
-                    <a
-                      className={styles.sociallink}
-                      href={sociallink.URL}
-                      target="_blank"
-                    >
-                      {sociallink.Account}
-                    </a>
-                  </li>
-                ))}
-              </ul>
+              </div>
             </div>
-          </div>
-        </section>
-        <section className={`${styles.section} ${styles.editions}`}>
-          <div className={styles.inner}>
-            <span className={styles.label}>Éditions Magazine</span>
-            <div className={styles.content}>
-              <PortableText value={editions} />
-            </div>
-          </div>
-        </section>
-        <section className={`${styles.section} ${styles.clients}`}>
-          <div className={styles.inner}>
-            <span className={styles.label}>Selected Clients</span>
-            <div className={styles.content}>
-              <PortableText value={selectedclients} />
-              <ul>
-                {clients.map((client, i) => (
-                  <li key={i}>
-                    {client.url ? (
-                      <a target="_blank" href={client.url}>
-                        {client.name}
+          </section>
+          <section className={`${styles.section} ${styles.socials}`}>
+            <div className={styles.inner}>
+              <span className={styles.label}>Now Online</span>
+              <div className={styles.content}>
+                <ul>
+                  {instagram.map((instalink, i) => (
+                    <li key={i}>
+                      <a
+                        className={styles.instalink}
+                        href={instalink.URL}
+                        target="_blank"
+                      >
+                        {instalink.Account}
                       </a>
-                    ) : (
-                      client.name
-                    )}
-                  </li>
-                ))}
-              </ul>
+                    </li>
+                  ))}
+                </ul>
+
+                <ul>
+                  {sociallinks.map((sociallink, i) => (
+                    <li key={i}>
+                      <a
+                        className={styles.sociallink}
+                        href={sociallink.URL}
+                        target="_blank"
+                      >
+                        {sociallink.Account}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
             </div>
-          </div>
-        </section>
+          </section>
+          <section className={`${styles.section} ${styles.editions}`}>
+            <div className={styles.inner}>
+              <span className={styles.label}>Éditions Magazine</span>
+              <div className={styles.content}>
+                <PortableText value={editions} />
+              </div>
+            </div>
+          </section>
+          <section className={`${styles.section} ${styles.clients}`}>
+            <div className={styles.inner}>
+              <span className={styles.label}>Selected Clients</span>
+              <div className={styles.content}>
+                <PortableText value={selectedclients} />
+                <ul>
+                  {clients.map((client, i) => (
+                    <li key={i}>
+                      {client.url ? (
+                        <a target="_blank" href={client.url}>
+                          {client.name}
+                        </a>
+                      ) : (
+                        client.name
+                      )}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </section>
+        </article>
+        <aside className={styles.stickynav}>
+          <h5>NEW BUSINESS</h5>
+          <p>
+            <a href="mailto:new@collect.nyc">new@collect.nyc</a>
+            <br />
+            +1 718 902 4911
+          </p>
+          <h5>WORK OPPORTUNITIES</h5>{" "}
+          <p>
+            <a href="mailto:info@collect.nyc">info@collect.nyc</a>
+            <br />
+            <a target="_blank" href="https://instagram.com/collect.nyc">
+              @collect.nyc
+            </a>
+          </p>{" "}
+          <h5>NOW ONLINE</h5>{" "}
+          <p>
+            <a target="_blank" href="https://instagram.com/collect.nyc">
+              Instagram
+            </a>
+            <br />
+            <a target="_blank" href="https://www.threads.net/@collect.nyc">
+              Threads
+            </a>
+            <br />
+            <a
+              target="_blank"
+              href="https://www.linkedin.com/company/collect-nyc"
+            >
+              LinkedIn
+            </a>
+          </p>
+          <h5>ÉDITIONS MAGAZINE</h5>
+          <p>
+            <a target="_blank" href="https://www.instagram.com/editions.mag/">
+              @editions.mag
+            </a>
+            <br />
+            <a target="_blank" href="https://editionsnewyork.com">
+              editionsnewyork.com
+            </a>
+          </p>
+          <h5>MAILBOX</h5>{" "}
+          <p>
+            100A Broadway #377
+            <br /> Brooklyn, NY 11249
+          </p>
+        </aside>
       </main>
-      <aside className={styles.stickynav}>
-        NEW BUSINESS new@collect.nyc +1 718 902 4911 WORK OPPORTUNITIES
-        info@collect.nyc @collect.nyc NOW ONLINE Instagram Threads LinkedIn
-        ÉDITIONS MAGAZINE @editions.mag editionsnewyork.com MAILBOX 100A
-        Broadway #377 Brooklyn, NY 11249
-      </aside>
+
       <Footer />
     </div>
   );
