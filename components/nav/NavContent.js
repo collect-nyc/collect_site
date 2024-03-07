57476ae20c313a6988b0b1a27beff33cdb01e76b
Joshua Tuscan <hello@joshuatuscan.com>
2024-03-07 14:42:56 -0500

    cleaning up

--- components/nav/NavContent.js
+++ components/nav/NavContent.js
@@ -176,9 +176,6 @@ const NavContent = ({ page, count, newCount, globalContent, showNav }) => {
               className={`${page === "services" && styles.current} ${
                 !showNav ? styles.hide : styles.show
               }`}
-              // onClick={() => {
-              //   setMobileMenuOpen(false);
-              // }}
               href={"/services"}
             >
               Agency Services
@@ -190,9 +187,6 @@ const NavContent = ({ page, count, newCount, globalContent, showNav }) => {
                 !showNav ? styles.hide : styles.show
               }`}
               href={"/about"}
-              // onClick={() => {
-              //   setMobileMenuOpen(false);
-              // }}
             >
               Get in Touch
             </Link>
@@ -210,64 +204,7 @@ const NavContent = ({ page, count, newCount, globalContent, showNav }) => {
             </a>
           </li>
         </ul>
-        {/* <footer className={styles.footer}>
-          <div className={styles.top_border} />
-          <div className={styles.contact}>
-            <div className={styles.col}>
-              <span className={styles.address}>
-                100A Broadway #377
-                <br /> Brooklyn, NY 11249
-              </span>
-              <div className={styles.social}>
-                <a
-                  href="https://www.instagram.com/collect.nyc/"
-                  target="_blank"
-                  rel="noreferrer"
-                  className={styles.insta}
-                >
-                  @collect.nyc
-                </a>
-                <a
-                  href="https://www.instagram.com/shop.editions/"
-                  target="_blank"
-                  rel="noreferrer"
-                  className={styles.insta}
-                >
-                  @shop.editions
-                </a>
-              </div>
-            </div>
 
-            <div className={styles.col}>
-              <div className={styles.reachout}>
-                <span className={styles.phone}>
-                  <a href="mailto:info@collect.nyc">info@collect.nyc</a>
-                </span>
-                <span className={styles.phone}>+1 718 902 4911</span>
-              </div>
-              <div className={styles.extra}>
-                <Link
-                  onClick={() => {
-                    setNavOpen(false);
-                    setMobileMenuOpen(false);
-                  }}
-                  href="/info/privacy"
-                >
-                  Privacy
-                </Link>
-                <Link
-                  onClick={() => {
-                    setNavOpen(false);
-                    setMobileMenuOpen(false);
-                  }}
-                  href="/info/impressum"
-                >
-                  Impressum
-                </Link>
-              </div>
-            </div>
-          </div>
-        </footer> */}
         <span className={styles.copyright}>
           ©{new Date().getFullYear()} Collect NEW YORK
         </span>
